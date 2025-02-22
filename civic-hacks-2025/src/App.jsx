64613170c8d6991af82317d12d853608abd4fdde
Quinn Flanigan <qinflanigan@gmail.com>
2025-02-22 16:49:40 -0500

    Filename update

--- civic-hacks-2025/src/App.jsx
+++ civic-hacks-2025/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import getLocation from './geography/geo-client'
+import getLocation from './geography/getCurrentLocation'
 
 function App() {
   
