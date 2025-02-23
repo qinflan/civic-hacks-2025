bee91e22a50dfc8517c7c4ce03ae8ecc27019451
Quinn Flanigan <qinflanigan@gmail.com>
2025-02-23 07:02:48 -0500

    finish-feat

--- civic-hacks-2025/src/geography/getCurrentLocation.js
+++ civic-hacks-2025/src/geography/getCurrentLocation.js
@@ -5,18 +5,12 @@ const getLocation = async () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          const latitudeRange = {
-            lat1: Math.round(position.coords.latitude * 10) / 10,
-            lat2: Math.round(position.coords.latitude * 10) / 10 + 0.5
-          };
-
-          const longitudeRange = {
-            lon1: Math.round(position.coords.longitude * 10) / 10,
-            lon2: Math.round(position.coords.longitude * 10) / 10 + 0.5
-          };
-
-          console.log("Latitude Range: ", latitudeRange, "Longitude Range: ", longitudeRange);
-          resolve({ lat: latitudeRange, lon: longitudeRange }); 
+            const lat1 = Math.round(position.coords.latitude * 10) / 10
+            const lat2 = Math.round(position.coords.latitude * 10) / 10 + 0.5
+            const lon1 = Math.round(position.coords.longitude * 10) / 10
+            const lon2 = Math.round(position.coords.longitude * 10) / 10 + 0.5
+          console.log(lat1, lat2, lon1, lon2);
+          resolve({ lat1, lat2, lon1, lon2}); 
         },
         (error) => {
           reject(error); 
