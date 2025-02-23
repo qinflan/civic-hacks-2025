652ba553889f3fe5c893d5f75e9c2089821d240f
Jerry Teixeira <166074504+JerryBLT@users.noreply.github.com>
2025-02-23 01:18:51 -0500

    Update AboutPage.jsx

--- civic-hacks-2025/src/components/AboutPage.jsx
+++ civic-hacks-2025/src/components/AboutPage.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>Species Explorer</h2>
+      <p>This application uses the GBIF Species API to display information about various species.</p>
+      <p>GBIF (Global Biodiversity Information Facility) is an international network and data infrastructure funded by the world's governments and aimed at providing anyone, anywhere, open access to data about all types of life on Earth.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
