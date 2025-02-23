b723755b7251870e8f34125a97c615ae95629f5a
HungHsu(Allen) Chen <chhchen@bu.edu>
2025-02-22 18:28:54 -0800

    getOccurence

---
+++ civic-hacks-2025/src/API/getOccurrenceByGeo.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+// returns the array of species based on the lat and lon
+export default function getOccurrenceByGeo(lat, lon, setData) {
+    const BASE_URL = 'https://api.gbif.org/v1/';
+
+    axios.get(BASE_URL + 'occurrence/search/?', {
+        params: {
+            decimalLongitude: `${lon.lon1}, ${lon.lon2}`,
+            decimalLatitude: `${lat.lat1}, ${lat.lat2}`,
+            limit: 30,
+            coordinateUncertaintyInMeters: "0,50",
+        }
+    }).then(
+        res => {
+            console.log(res.data.results);
+            setData(res.data.results);
+        }
+    ).catch(e => {
+        console.log(e);
+    })
+  }
